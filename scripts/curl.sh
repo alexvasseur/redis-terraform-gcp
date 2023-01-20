f8c50711f1c8e043a5dff56c33e8234bf2172ed5
Alexandre Vasseur <avasseur@gmail.com>
2023-01-20 15:23:03 +0100

    CRDB json

--- scripts/curl.sh
+++ scripts/curl.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# ./curl.sh password file.json
+# ./curl.sh $PASS file.json
+
+URI="v1/bdbs"
+if [ $2 = "db_crdb.json" ]
+then
+  URI="v1/crdbs"
+fi
+echo $URI
+
 
 curl -X POST \
-  https://cluster.avasseur-default.demo.redislabs.com:9443/v1/bdbs \
+  https://cluster.avasseur-default.demo.redislabs.com:9443/$URI \
   -H 'Content-Type:application/json' \
   -u admin@redis.io:$1 \
   -d @$2 \
