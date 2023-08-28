6142207285a6e26021a667792816c37d05312429
Alexandre Vasseur <avasseur@gmail.com>
2023-08-28 15:22:46 +0200

    default to 7.2 GA

--- variable.tf
+++ variable.tf
@@ -21,7 +21,8 @@ variable "clustersize" {
 
 // other possible edits *************************************
 variable "RS_release" {
-  default = "https://s3.amazonaws.com/redis-enterprise-software-downloads/6.4.2/redislabs-6.4.2-81-focal-amd64.tar"
+  default = "https://s3.amazonaws.com/redis-enterprise-software-downloads/7.2.4/redislabs-7.2.4-52-focal-amd64.tar"
+  #default = "https://s3.amazonaws.com/redis-enterprise-software-downloads/6.4.2/redislabs-6.4.2-81-focal-amd64.tar"
   #"https://s3.amazonaws.com/redis-enterprise-software-downloads/6.2.18/redislabs-6.2.18-65-bionic-amd64.tar"
 }
 variable "project" {
